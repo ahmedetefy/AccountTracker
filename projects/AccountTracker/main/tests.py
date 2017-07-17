8c7cce431ff84ac18135bee6a8728f780d90f6d5
Ahmed Etefy <ahmed.etefy12@gmail.com>
2017-07-17 13:01:27 +0200

    Added AccountModelTest to main/tests.py

--- projects/AccountTracker/main/tests.py
+++ projects/AccountTracker/main/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Account
+
+
+class AccountModelTest(TestCase):
+
+    def test_string_representation(self):
+        account = Account(first_name="Ahmed")
+        self.assertEqual(str(account), account.first_name)
+
+    def test_verbose_name_plural(self):
+        self.assertEqual(str(Account._meta.verbose_name_plural), "accounts")
+
+    def test_get_absolute_url(self):
+        account = Account.objects.create(
+            first_name="Ahmed", last_name="Etefy",
+            iban_number="GB04BARC20474473160944")
+        self.assertIsNotNone(account.get_absolute_url())
