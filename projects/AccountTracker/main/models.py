1ad792ecb2f6c31137669cd624830a476b6991ce
Ahmed Etefy <ahmed.etefy12@gmail.com>
2017-07-17 12:44:37 +0200

    Created Model Account in main/models.py

--- projects/AccountTracker/main/models.py
+++ projects/AccountTracker/main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.core.urlresolvers import reverse
+from localflavor.generic.models import IBANField
 
-# Create your models here.
+
+class Account(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    iban_number = IBANField()
+
+    def __unicode__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('main:account_edit', kwargs={'pk': self.pk})
